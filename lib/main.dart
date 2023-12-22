1740a50afb6a5ad1bc4eaca3210a10b5403b4dc2
Aaron Evanjulio <aaronevanjulio18@gmail.com>
2023-12-22 08:37:42 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sample_test/providers/done_module_provider.dart';
+import 'package:sample_test/ui/module_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => DoneModuleProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const ModulePage(),
+      ),
+    );
+  }
+}
